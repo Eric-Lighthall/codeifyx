b86bad30d7f3ead1436893dbb0cd9c897d8c904f
Eric Lighthall <55029902+Eric-Lighthall@users.noreply.github.com>
2024-07-19 15:45:48 -0700

    update: email typescript conversion

--- codeifyx-next/src/utils/email.ts
+++ codeifyx-next/src/utils/email.ts
@@ -1,15 +1,20 @@
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+
 const mailgun = new Mailgun(formData);
 
 const mg = mailgun.client({
     username: 'api',
-    key: process.env.MAILGUN_API_KEY
+    key: process.env.MAILGUN_API_KEY || ''
 });
 
-const DOMAIN = process.env.MAILGUN_DOMAIN;
+const DOMAIN = process.env.MAILGUN_DOMAIN || '';
 
-const sendEmail = async (to, subject, text, html) => {
+const sendEmail = async (to: string, subject: string, text: string, html: string): Promise<any> => {
+    if (!process.env.MAILGUN_API_KEY || !DOMAIN) {
+        throw new Error('Mailgun API key or domain not set in environment variables');
+    }
+
     try {
         const msg = await mg.messages.create(DOMAIN, {
             from: "Codeifyx <verification@codeifyx.com>",
@@ -26,4 +31,4 @@ const sendEmail = async (to, subject, text, html) => {
     }
 };
 
-module.exports = sendEmail;
+export default sendEmail;
